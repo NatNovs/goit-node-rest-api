ab1be4b56223d355886d5a93f320088e709ff9a6
natnovs <136241094+NatNovs@users.noreply.github.com>
2024-04-01 14:37:48 +0200

    21

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
+const express = require('express')
+const logger = require('morgan')
+const cors = require('cors')
 
-import contactsRouter from "./routes/contactsRouter.js";
+const contactsRouter = require('./routes/api/contacts')
 
-const app = express();
+const app = express()
 
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use(morgan("tiny"));
-app.use(cors());
-app.use(express.json());
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
 
-app.use("/api/contacts", contactsRouter);
+app.use('/api/contacts', contactsRouter)
 
-app.use((_, res) => {
-  res.status(404).json({ message: "Route not found" });
-});
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' })
+})
 
 app.use((err, req, res, next) => {
-  const { status = 500, message = "Server error" } = err;
-  res.status(status).json({ message });
-});
+  res.status(500).json({ message: err.message })
+})
 
-app.listen(3000, () => {
-  console.log("Server is running. Use our API on port: 3000");
-});
+module.exports = app
